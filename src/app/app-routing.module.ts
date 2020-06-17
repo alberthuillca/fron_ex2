e9da5c3ddb2867224b673f627bfab1a7f6eaa7c8
unknown <richeti1998@gmail.com>
2020-06-16 23:47:47 -0500

    uno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductoComponent } from './components/producto/producto.component';
+import { CategoriaComponent } from './components/categoria/categoria.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'producto', component: ProductoComponent},
+  {path: 'categoria', component: CategoriaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { CategoriaComponent } from './components/categoria/categoria.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent,
+    CategoriaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
