23018f46a6f345adfce223f3c565ee910c4d8a78
unknown <richeti1998@gmail.com>
2020-06-17 01:00:13 -0500

    dos

---
+++ src/app/service/producto.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Producto } from '../Modelo/Producto';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+  
+})
+export class ProductoService {
+  
+
+  constructor(private http:HttpClient, private router:Router) { }
+
+
+  listaProducto(): Observable<Producto[]>{
+    return this.http.get<Producto[]>(`${ environment.apiUrl }/productos/`);
+  }
+  crearProducto(producto:Producto){
+    return this.http.post<Producto>(`${ environment.apiUrl }/productos/add`,producto);
+  }
+  deleteProducto(producto:Producto){
+   return this.http.put<Producto>(`${ environment.apiUrl }/productos/D`, producto);
+
+  }
+  editarProducto(producto:Producto){
+   return this.http.put<Producto>(`${ environment.apiUrl }/productos/ `+producto.idproducto,producto);
+  
+  }
+  
+}
